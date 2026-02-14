dcd97dabc0f9292675942cfa1dc4572a2ee36e73
Chi <test@gmail.com>
2026-02-14 18:11:39 +0800

    2/14-1

---
+++ Hartals.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int test,day,party;
+	scanf("%d",&test);
+	
+	for (int t=0;t<test;t++){
+		
+		scanf("%d",&day);
+		scanf("%d",&party);
+		int hartal[party];
+		for (int i=0;i<party;i++){
+			scanf("%d",hartal[i]);
+		}
+		
+		
+		
+	}
+	
+	return 0;
+}
